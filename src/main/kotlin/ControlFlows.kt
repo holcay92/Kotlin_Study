4fe0c91c58d26a2491f8dfa58cc36d44ec42e6f8
H.Halil Olcay <81256755+holcay92@users.noreply.github.com>
2022-06-08 00:53:49 +0300

    control flows println detail added

--- src/main/kotlin/ControlFlows.kt
+++ src/main/kotlin/ControlFlows.kt
@@ -2,11 +2,11 @@ fun main(){
     print("enter a grade: ")
     var grade:Int = readLine()?.toInt() ?: 0
     when(grade){
-        in 0..59 -> println("F")
-        in 60..69 -> println("D")
-        in 70..79 -> println("C")
-        in 80..89 -> println("B")
-        in 90..100 -> println("A")
+        in 0..59 -> println("Grade: F")
+        in 60..69 -> println("Grade: D")
+        in 70..79 -> println("Grade: C")
+        in 80..89 -> println("Grade: B")
+        in 90..100 -> println("Grade: A")
         else -> println("Invalid grade")
     }
     println("comparing: ${grade.compareTo(50)} ")
