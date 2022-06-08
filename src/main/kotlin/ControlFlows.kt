a1886711fa72860db438e4e3c248054dc36abb4f
H.Halil Olcay <81256755+holcay92@users.noreply.github.com>
2022-06-08 21:21:51 +0300

    control flows print detail added

--- src/main/kotlin/ControlFlows.kt
+++ src/main/kotlin/ControlFlows.kt
@@ -1,6 +1,7 @@
 fun main(){
     print("enter a grade: ")
     var grade:Int = readLine()?.toInt() ?: 0
+
     when(grade){
         in 0..59 -> println("Grade: F")
         in 60..69 -> println("Grade: D")
@@ -10,6 +11,19 @@ fun main(){
         else -> println("Invalid grade")
     }
     println("comparing: ${grade.compareTo(50)} ")
+    println("--------------------------------------------\n enter country code: ")
+
+    val countryCode=readLine()!!
+    when(countryCode.lowercase()){
+        "tr","az"-> println("Turk Vatandas")
+        "ar"->println("Arab Vatandası")
+        "us" -> println("United States")
+        "fr"->println("France Vatandası")
+        "ru"->println("Russian Vatandası")
+        "uk"->println("United Kingdom Vatandası")
+        else -> println("Invalid country code")
+    }
+    println("--------------------------------------------\nyes or no")
 
     val answer = readLine()!!
     if(answer.contains("Yes",ignoreCase = true)){
@@ -18,6 +32,7 @@ fun main(){
     else{
         println("its a no!!")
     }
+    println("--------------------------------------------")
 
     val result: String = if(answer.lowercase()=="yes") {
         "öğrenci"
@@ -25,4 +40,21 @@ fun main(){
         "öğrenci değil"
     }
     println(result)
+    println("--------------------------------------------")
+
+    // state kullanimi
+    if(answer.contains("Evet",ignoreCase=true)){
+        println("Öğrenci")
+    }else{
+        println("Öğrenci Değil")
+    }
+    println("--------------------------------------------")
+
+// expression kullanimi.{icerisindeki son satir,deger olarak degiskene atanir.
+    val result1:String=if(answer =="Evet"){
+        "Öğrenci"
+
+
+    }else{"Öğrenci Değil"
+    }
 }
